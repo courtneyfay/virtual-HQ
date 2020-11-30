f6965007ec35fd8fd6e1d8b0c94cfd81e27c8fc7
Courtney Fay <fay.courtney@gmail.com>
2020-11-30 14:26:25 -0700

    adds a link to the cube that opens when the cube is clicked

--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,7 @@
 ////////////////////////////////////////////////////////////////
-// GLOBAL CONSTANTS
+// GLOBAL VARIABLES
 ////////////////////////////////////////////////////////////////
-let renderer;
-let scene;
-let camera;
+let renderer, scene, camera, raycaster, object;
 
 function init() {
   // set up the renderer
@@ -29,7 +27,7 @@ function init() {
   );
 }
 
-function createBox(url) {
+function createCube(url) {
   // box geometry allows for a cube object
   const geometry = new THREE.BoxGeometry();
 
@@ -42,14 +40,11 @@ function createBox(url) {
   // a mesh is an object that takes a geometry, and applies a material to it
   // which we can then insert into our scene and move freely around
   const cube = new THREE.Mesh(geometry, material);
-  /*var object = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
-    color: Math.random() * 0xffffff }));
-  object.userData = { URL: "http://stackoverflow.com"}; */
-  // if (intersects.length > 0) {
-  //   window.open(intersects[0].object.userData.URL);
-  // }
-  // cube.userData = { URL: url };
-  console.log("cube", cube);
+
+  // adds the URL to the cube
+  cube.userData = { URL: url };
+
+  // adds the cube to the scene
   scene.add(cube);
 
   // by default, when scene.add is called, the object is added to the coordinates (0,0,0)
@@ -57,26 +52,27 @@ function createBox(url) {
   // so moving the camera out a bit avoids this
   camera.position.z = 5;
 
-  // document.addEventListener("mousedown", onDocumentMouseDown, false);
+  object = cube;
+
+  document.addEventListener("mousedown", onDocumentMouseDown, false);
 }
 
 function onDocumentMouseDown(event) {
   event.preventDefault();
-  console.log("onDocumentMouseDown", event);
-  var vector = new THREE.Vector3(
+
+  // set up vector
+  const vector = new THREE.Vector3(
     (event.clientX / window.innerWidth) * 2 - 1,
     -(event.clientY / window.innerHeight) * 2 + 1,
     0.5
   );
-  console.log("vector", vector);
-  projector.unprojectVector(vector, camera);
-  var raycaster = new THREE.Raycaster(
+
+  // set up the raycaster to detect if mouse clicked on object
+  raycaster = new THREE.Raycaster(
     camera.position,
     vector.sub(camera.position).normalize()
   );
-  console.log("raycaster", raycaster);
-  var intersects = raycaster.intersectObjects(objects);
-  console.log("intersects", intersects);
+  const intersects = raycaster.intersectObject(object);
   if (intersects.length > 0) {
     window.open(intersects[0].object.userData.URL);
   }
@@ -101,11 +97,11 @@ function render() {
   renderer.render(scene, camera);
 }
 
-function run() {
+function start() {
   init();
-  createBox("https://stackoverflow.com/");
+  createCube("https://stackoverflow.com/");
   animate();
   render();
 }
 
-run();
+start();
